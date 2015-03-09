856f916afc2631518b2283a6e29922fb7ea99002
urandu <bildadnamawa@gmail.com>
2015-03-09 23:25:02 +0300

    step 2

---
+++ convert.php
@@ -0,0 +1,15 @@
+<?PHP
+
+	$string=$_POST["textstring"];
+	$stringArray=str_split($string);
+	$b="";
+	foreach($stringArray as $str)
+	{
+		$new=ord($str);
+		$new+=0;
+		$b.=decbin($new);
+		$b.=" ";
+	}
+include("index.php");
+?>
+
