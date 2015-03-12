ae7cc8d70fa3383f65195da2d96cbf417c83d142
urandu <bildadnamawa@gmail.com>
2015-03-12 23:48:40 +0300

    final code

--- trevnoc.php
+++ trevnoc.php
@@ -1,5 +1,9 @@
 <?php
-
+/*
+written by urandu bildad namawa email:bildadnamawa@gmail.com website:http://www.urandu.foundit.co.ke
+this code is for free and you are free to modify it and play around with it.you can also add new ideas and help fix the bugs.
+*/
+#conversion from binary to text
 	$bildad="";
 	$nw=explode(" ",$_POST['textstring']);
 
